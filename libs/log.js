e41647ec6feccc1766014e6c95242a113afdf068
Dr_Z <yuriy.borbich@pmscloud.com>
2016-06-01 15:50:02 +0300

    fix logger info

--- libs/log.js
+++ libs/log.js
@@ -6,7 +6,7 @@ var ENV  = process.env.NODE_ENV;
 function getLogger(module){
     var path = module.filename.split('/').slice(-2).join('/');
     return new winston.Logger({
-        transport : [
+        transports: [
             new winston.transports.Console({
                 colorize : true,
                 level : ENV == 'development' ? 'debug' : 'error',
