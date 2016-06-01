0e306b4fb7dcb4fb7ad15009499e088ba79e1a81
Dr_Z <yuriy.borbich@pmscloud.com>
2016-06-01 15:59:21 +0300

    fix logger info1

--- libs/log.js
+++ libs/log.js
@@ -4,7 +4,7 @@
 var winston = require('winston');
 var ENV  = process.env.NODE_ENV;
 function getLogger(module){
-    var path = module.filename.split('/').slice(-2).join('/');
+    var path = module.filename.split('\\').slice(-2).join('/');
     return new winston.Logger({
         transports: [
             new winston.transports.Console({
